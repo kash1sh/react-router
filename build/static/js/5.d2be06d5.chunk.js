1dd609b6a44d11db47475147898aa714c66cb265
Kashish <kashishchugh2001@gmail.com>
2021-11-23 18:33:12 +0530

    Rremoved github folder from repo

--- build/static/js/5.d2be06d5.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpreact-complete-guide"]=this["webpackJsonpreact-complete-guide"]||[]).push([[5],{36:function(e,t,c){"use strict";c.r(t);var n=c(1);t.default=function(){return Object(n.jsx)("div",{className:"centered",children:Object(n.jsx)("p",{children:"Page not found!"})})}}}]);
-//# sourceMappingURL=5.d2be06d5.chunk.js.map
